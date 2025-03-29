2068d8de37ec120342bbccc606544f4c27ee6dd3
Dungprj <doibatcong2003@gmail.com>
2025-03-29 18:21:20 +0700

    add redis cloud

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,9 +5,18 @@ const redisClient = redis.createClient({
     url: process.env.REDIS_HOST || 'redis://localhost:6379' // Mặc định localhost, port 6379
 });
 
+//redis cloud
+// const redisClient = redis.createClient({
+//     username: process.env.REDIS_USERNAME,
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//         host: process.env.REDIS_HOST,
+//         port: process.env.REDIS_PORT
+//     }
+// });
 // Xử lý sự kiện
 redisClient.on('connect', () => {
-    console.log('Connected to Redis on localhost');
+    console.log('Connected to Redis :', process.env.REDIS_HOST);
 });
 
 redisClient.on('error', err => {
